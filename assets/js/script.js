5b4c174c0f4dd998f8f216c87764b960a7bf8373
sush-tech <sushmithar31@gmail.com>
2023-09-30 10:49:27 -0400

    adding genres to the dropdown menu using API call response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // This will return information about whatever movie is searched using t=[movie_title] fast and furious 6 is an example.
 // Need to figure out how to use search input to change that variable
 // This is for the omdb api
+var genre_dropdown = document.getElementById("genre-select");
 var requestUrl = 'http://www.omdbapi.com/?t=Fast&Furious6&apikey=806e9e92';
 
 fetch(requestUrl)
@@ -15,14 +16,26 @@ fetch(requestUrl)
 
 //This is used to pull a list of movie genres from tmdb api
 const options = {
-    method: 'GET',
+    method: 'GET',  //http method,GET PUT POST DELETE 
     headers: {
       accept: 'application/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzg5MzUzNzVhNGVmMTY0YWY0ZDliYTdhYWVhMGQ1YSIsInN1YiI6IjY1MTM4NTNmYzUwYWQyMDBjOTE4YzgzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SrcgoM8n2F5rggdya7kCA2Ca6_FV9OAtuXEkJpiXPBE'
     }
 };
   
-fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)  //url endpoint
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+      //console.log(response)
+      var genres = response.genres;
+      //console.log(genres);
+      for( var i=0;i<genres.length ; i++)
+      {
+        // console.log(genres[i].name);
+        var optionEl = document.createElement("option");
+        optionEl.textContent = genres[i].name;
+        optionEl.value = genres[i].name;
+        genre_dropdown.append(optionEl);
+      }
+    })
     .catch(err => console.error(err));
