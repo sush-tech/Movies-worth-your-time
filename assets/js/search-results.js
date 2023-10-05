bd19d2f2eb4489106bf30b16e11bb6bd5b9622ba
Silverfoot42 <elliottgc@gmail.com>
2023-10-05 19:38:26 -0400

    added code to display omdb api details to the search results page

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,6 +1,18 @@
 var backButton = document.getElementById('backbtn');
+// Got this from the ai learning assistant
 var searchParams = new URLSearchParams(window.location.search);
 var movie = searchParams.get('q');
+var titleEl = document.querySelector('#title');
+var plotEl = document.querySelector('#plot');
+var criticEl = document.querySelector('#critic-review');
+
+const options = {
+  method: 'GET',  //http method,GET PUT POST DELETE 
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzg5MzUzNzVhNGVmMTY0YWY0ZDliYTdhYWVhMGQ1YSIsInN1YiI6IjY1MTM4NTNmYzUwYWQyMDBjOTE4YzgzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SrcgoM8n2F5rggdya7kCA2Ca6_FV9OAtuXEkJpiXPBE'
+  }
+};
 
 fetch('http://www.omdbapi.com/?t=' + movie + '&apikey=806e9e92')
       .then(function (response) {
@@ -8,6 +20,9 @@ fetch('http://www.omdbapi.com/?t=' + movie + '&apikey=806e9e92')
       })
       .then(function (data) {
         console.log(data);
+        titleEl.textContent = "Movie Title: " + data.Title;
+        plotEl.textContent = data.Plot;
+        criticEl.textContent = "Metacritic Critic Review Score: " + data.Metascore;
       })
       .catch(function (error) {
         console.error(error);
@@ -16,3 +31,5 @@ fetch('http://www.omdbapi.com/?t=' + movie + '&apikey=806e9e92')
 backButton.addEventListener("click", function() {
     window.location.href = "./index.html";
 });
+
+
