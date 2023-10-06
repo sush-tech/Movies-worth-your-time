831b557f26a741aa97f6675d750b79f04a66ef82
crashley12 <142274436+crashley12@users.noreply.github.com>
2023-10-05 21:39:22 -0400

    Update search.js
    Signed-off-by: crashley12 <142274436+crashley12@users.noreply.github.com>

--- assets/js/search.js
+++ assets/js/search.js
@@ -10,8 +10,22 @@ var movieSearch = function (event) {
     var movie = movieTitle.value.trim();
     var genre = genre_dropdown.value;
     var requestUrl = 'http://www.omdbapi.com/?t=' + movie + '&apikey=806e9e92';
+    var requestUrl2 = 'https://api.themoviedb.org/3/genre/movie/list?language=en' + movie + '&apikey=c78935375a4ef164af4d9ba7aaea0d5a'
+
     if (movie != "") {
       fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        var queryString = './search-results.html?q=' + encodeURIComponent(movie);
+        location.assign(queryString);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+        fetch(requestUrl2)
       .then(function (response) {
         return response.json();
       })
