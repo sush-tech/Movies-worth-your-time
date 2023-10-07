6fb93fc64bdd1fea0e9c3704c7d7b64c9d523445
Silverfoot42 <elliottgc@gmail.com>
2023-10-07 01:10:23 -0400

    made it so data appears on the table in the favorites page and whe you click on a title it links to the corresponding movie page. removed the gfg css from the page because the links wouldn't function otherwise

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -11,29 +11,50 @@ function displayFavorites() {
   
     // Display the list of favorited movie titles on the favorites page
     favorites.forEach(movie => {
-      var rowEl = document.createElement('tr')
-      var titleElement = document.createElement('th');
-      titleElement.innerHTML = movie;
-      titleElement.addEventListener('click', () => {
+      var rowEl = document.createElement('tr');
+      var titleEl = document.createElement('th');
+      var genreEl = document.createElement('td');
+      var popularityEl = document.createElement('td');
+      var moviePage = document.createElement('a');
+      moviePage.href = './search-results.html?q=' + encodeURIComponent(movie);
+      moviePage.innerHTML = movie;
+
+      moviePage.addEventListener('click', function(event) {
+        event.preventDefault();
         var queryString = './search-results.html?q=' + encodeURIComponent(movie);
         location.assign(queryString);
       });
+
+      fetch('https://www.omdbapi.com/?t=' + movie + '&apikey=806e9e92')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        genreEl.innerHTML = data.Genre;
+      })
+      .catch(function (error) {
+        console.error(error);
+        });
+
+      fetch('https://api.themoviedb.org/3/search/movie?api_key=c78935375a4ef164af4d9ba7aaea0d5a&query=' + movie + '&include_adult=false&language=en-US&page=1')
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          popularityEl.innerHTML = data.results[0].popularity;
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
       favTable.appendChild(rowEl);
-      rowEl.appendChild(titleElement);
+      rowEl.appendChild(titleEl);
+      rowEl.appendChild(genreEl);
+      rowEl.appendChild(popularityEl);
+      titleEl.appendChild(moviePage);
     });
 }
-
-// Function to retrieve the movie data by title
-/*function getMovieDataByTitle(title) {
-    // Retrieve the favorites array from local storage
-    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  
-    // Find the movie object with the matching title
-    let movie = favorites.find(movieTitle => movieTitle === title);
-  
-    // Return the movie object
-    return movie;
-}*/
 
 backButton.addEventListener("click", function() {
     window.location.href = "./index.html";
